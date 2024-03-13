e46257d0f7188a6efe903eacb3bf73d0e3fe3f5b
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-03-13 19:47:15 -0300

    Update Calc.java (2.0)

--- POO (function)/Função Calculadora/Calc.java
+++ POO (function)/Função Calculadora/Calc.java
@@ -1,35 +1,24 @@
-public class Calc {
-    public static void Soma(int x,int y){
-        System.out.println( x+y );
-}
-    public static void Subtração(int x, int y){
-        System.out.println(x-y);
-}
-    public static void Divisão(double x, double y){
-        System.out.println(x/y);
-}
-    public static void Multiplicação(int x, int y){
-        System.out.println(x*y);
-}
+public class Calculadora {
     public static void main(String[] args) {
+        Calc();
+    }
+    public static void Calc(){
         System.out.println("Olá, bem vindo a calculadora aléatoria do PP");
         System.out.println("Os numeros e operadores são aléatorio");
-        int num1 =(int) (Math.random()* 12 + 1); int num2 = (int) (Math.random()* 12 +1);
+        double num1 =(double) (Math.random()* 12 + 1); double num2 = (double) (Math.random()* 12 +1);
         int escolha = (int) (Math.random() * 4 + 1);
-        switch (escolha) {
-            case 1:
-        Soma(num1,num2);
-        break;
-            case 2:
-        Subtração(num1,num2);
-        break;
-            case 3:
-        Divisão(num1,num2);
-        break;
-            case 4:
-        Multiplicação(num1, num2);
-        break;  
-    
+        
+        if(escolha == 1){
+            System.out.println("Resultado:\n" + (num1+num2));
+        }else if(escolha == 2){
+            System.out.println("Resultado:\n"+ (num1-num2));
+        }else if (escolha == 3){
+            System.out.println("Resultado:\n"+ (num1*num2));
+        }else if (escolha == 4){
+            System.out.println("Resultado:\n"+ (num1/num2));
+        }else{
+            System.out.println("opcão inválida");
         }
     }
-}
+ }
+
