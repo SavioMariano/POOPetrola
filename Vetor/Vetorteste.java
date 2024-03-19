920564286448be45e60fc9eba7875a32b226140b
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-03-19 13:54:50 -0300

    Delete Vetor directory

--- Vetor/Vetorteste.java
+++
@@ -1,9 +0,0 @@
-public class Vetorteste{
-    public static void main(String[] args){
-        int[] words = {1,2,3,4,5,6,7};
-        for (int word : words) {
-            System.out.println("Vowels in word '" + word + "':");
-            
-        }
-    }
-}
